751a67a5d21b12b490a7c3fb4ee2d2ba65353cc7
Sara Spink <spinkbot@gmail.com>
2017-10-20 13:52:02 -0700

    the list is a list

--- build/js/app.js
+++ build/js/app.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
 
     var promise = new Promise(function (resolve, reject) {
       var request = new XMLHttpRequest();
-      var url = 'https://api.betterdoctor.com/2016-03-01/doctors?query=' + complaint + '&location=45.4814320%2C-122.8016600%2C20&sort=rating-asc&skip=0&limit=40&user_key=' + apiKey;
+      var url = 'https://api.betterdoctor.com/2016-03-01/doctors?query=' + complaint + '&location=45.4814320%2C-122.8016600%2C50&sort=rating-asc&skip=0&limit=40&user_key=' + apiKey;
       request.onload = function () {
         if (this.status === 200) {
           resolve(request.response);
@@ -33,11 +33,13 @@ $(document).ready(function () {
         docArray.push(docs.profile);
       });
       docArray.forEach(function (names) {
-        docNames.push(names.first_name);
+        docNames.push(names.first_name, names.last_name);
       });
-
       $('.showDoctors').text('Here are some doctors that treat ' + complaint + '.');
-      $('.showDoctors').append('<li>' + docNames + '</li>');
+
+      docArray.forEach(function (names) {
+        $('#showDoctorNames').append('<li> ' + (names.first_name, names.last_name) + ' </li>');
+      });
       // $('.showTemp').text(`The temperature in Fahrenheit is ${body.main.temp} degrees.`);
     }, function (error) {
       $('.showErrors').text('There was an error processing your request: ' + error.message);
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
       let promise = new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
       let url =
-      `https://api.betterdoctor.com/2016-03-01/doctors?query=${complaint}&location=45.4814320%2C-122.8016600%2C20&sort=rating-asc&skip=0&limit=40&user_key=${apiKey}`;
+      `https://api.betterdoctor.com/2016-03-01/doctors?query=${complaint}&location=45.4814320%2C-122.8016600%2C50&sort=rating-asc&skip=0&limit=40&user_key=${apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
@@ -28,12 +28,14 @@ $(document).ready(function() {
         docArray.push(docs.profile)
         })
       docArray.forEach(function(names) {
-        docNames.push(names.first_name)
+        docNames.push(names.first_name, names.last_name)
         })
-
-
         $('.showDoctors').text(`Here are some doctors that treat ${complaint}.`);
-        $('.showDoctors').append(`<li>${docNames}</li>`)
+
+
+        docArray.forEach(function(names) {
+        $('#showDoctorNames').append(`<li> ${names.first_name, names.last_name} </li>`)
+          })
         // $('.showTemp').text(`The temperature in Fahrenheit is ${body.main.temp} degrees.`);
       }, function(error) {
         $('.showErrors').text(`There was an error processing your request: ${error.message}`);
