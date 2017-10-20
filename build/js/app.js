2b6b36502477a6bb689cc574e6e136e35c768c95
Sara Spink <spinkbot@gmail.com>
2017-10-19 21:07:46 -0700

    set up initial files and test api call

---
+++ build/js/app.js
@@ -0,0 +1,38 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+exports.apiKey = "1c2b9da6e9ab0586e608a21396513a66";
+
+},{}],2:[function(require,module,exports){
+'use strict';
+
+var apiKey = require('./../.env').apiKey;
+
+$(document).ready(function () {
+  $('#weatherLocation').click(function () {
+    var city = $('#location').val();
+    $('#location').val("");
+
+    var promise = new Promise(function (resolve, reject) {
+      var request = new XMLHttpRequest();
+      var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=Imperial&appid=' + apiKey;
+      request.onload = function () {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+
+    promise.then(function (response) {
+      var body = JSON.parse(response);
+      $('.showHumidity').text('The humidity in ' + city + ' is ' + body.main.humidity + '%');
+      $('.showTemp').text('The temperature in Fahrenheit is ' + body.main.temp + ' degrees.');
+    }, function (error) {
+      $('.showErrors').text('There was an error processing your request: ' + error.message);
+    });
+  });
+});
+
+},{"./../.env":1}]},{},[2]);
