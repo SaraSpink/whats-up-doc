a45565e318069fb4daf0f8dcd3922ad987e35219
Sara Spink <spinkbot@gmail.com>
2017-10-24 21:14:22 -0700

    successfully separate front and back end

--- build/js/app.js
+++ build/js/app.js
@@ -2,7 +2,81 @@
 exports.apiKey = "bb6d824019641768d2341bd792eef6ae";
 
 },{}],2:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Doctor = exports.Doctor = function () {
+  function Doctor() {
+    _classCallCheck(this, Doctor);
+  }
+
+  _createClass(Doctor, [{
+    key: "apiRequest",
+    value: function apiRequest(complaint, apiKey) {
+      var promise = new Promise(function (resolve, reject) {
+        var request = new XMLHttpRequest();
+        var url = "https://api.betterdoctor.com/2016-03-01/doctors?query=" + complaint + "&location=45.4814320%2C-122.8016600%2C50&sort=rating-asc&skip=0&limit=40&user_key=" + apiKey;
+        request.onload = function () {
+          if (this.status === 200) {
+            resolve(request.response);
+          } else {
+            reject(Error(request.statusText));
+          }
+        };
+        request.open("GET", url, true);
+        request.send();
+      });
+
+      promise.then(function (response) {
+        var body = JSON.parse(response);
+        var docArray = [];
+
+        body.data.forEach(function (doc) {
+          var name = doc.profile.first_name + " " + doc.profile.last_name + ", " + doc.profile.title;
+          var address = doc.practices[0].visit_address;
+          var formattedAddress = address.street + " <br> " + address.city + " <br> " + address.state + ", " + address.zip;
+          var phone = "" + doc.practices[0].phones.find(function (phone) {
+            return phone.type === "landline";
+          }).number;
+
+          docArray.push({
+            name: name,
+            address: formattedAddress,
+            phone: phone
+          });
+        });
+
+        console.log(docArray);
+        $('#showDoctorNames').text("");
+        if (body.data.length != 0) {
+          $('.showDoctors').text("Here are some doctors that treat " + complaint + ".");
+
+          docArray.forEach(function (doctor) {
+            $('#showDoctorNames').append("<li> <b>" + doctor.name + "</b> <br> " + doctor.address + " <br> " + doctor.phone + " </li>");
+          });
+        } else {
+          $(".showDoctors").text("There are no doctors that treat " + complaint + ", please modify your search terms");
+        }
+      }, function (error) {
+        $('.showErrors').text("There was an error processing your request: " + error.message);
+      });
+    }
+  }]);
+
+  return Doctor;
+}();
+
+},{}],3:[function(require,module,exports){
 'use strict';
+
+var _doctor = require('./../js/doctor.js');
 
 var apiKey = require('./../.env').apiKey;
 
@@ -10,55 +84,9 @@ $(document).ready(function () {
   $('#complaintButton').click(function () {
     var complaint = $('#complaint').val();
     $('#complaint').val("");
-
-    var promise = new Promise(function (resolve, reject) {
-      var request = new XMLHttpRequest();
-      var url = 'https://api.betterdoctor.com/2016-03-01/doctors?query=' + complaint + '&location=45.4814320%2C-122.8016600%2C50&sort=rating-asc&skip=0&limit=40&user_key=' + apiKey;
-      request.onload = function () {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      };
-      request.open("GET", url, true);
-      request.send();
-    });
-
-    promise.then(function (response) {
-      var body = JSON.parse(response);
-      var docArray = [];
-
-      body.data.forEach(function (doc) {
-        var name = doc.profile.first_name + ' ' + doc.profile.last_name + ', ' + doc.profile.title;
-        var address = doc.practices[0].visit_address;
-        var formattedAddress = address.street + ' <br> ' + address.city + ' <br> ' + address.state + ', ' + address.zip;
-        var phone = '' + doc.practices[0].phones.find(function (phone) {
-          return phone.type === "landline";
-        }).number;
-
-        docArray.push({
-          name: name,
-          address: formattedAddress,
-          phone: phone
-        });
-      });
-
-      console.log(docArray);
-      $('#showDoctorNames').text("");
-      if (body.data.length != 0) {
-        $('.showDoctors').text('Here are some doctors that treat ' + complaint + '.');
-
-        docArray.forEach(function (doctor) {
-          $('#showDoctorNames').append('<li> <b>' + doctor.name + '</b> <br> ' + doctor.address + ' <br> ' + doctor.phone + ' </li>');
-        });
-      } else {
-        $(".showDoctors").text('There are no doctors that treat ' + complaint + ', please modify your search terms');
-      }
-    }, function (error) {
-      $('.showErrors').text('There was an error processing your request: ' + error.message);
-    });
+    var doctor = new _doctor.Doctor();
+    doctor.apiRequest(complaint, apiKey);
   });
 });
 
-},{"./../.env":1}]},{},[2]);
+},{"./../.env":1,"./../js/doctor.js":2}]},{},[3]);
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,59 +1,11 @@
+import { Doctor } from './../js/doctor.js'
 let apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
   $('#complaintButton').click(function() {
     let complaint = $('#complaint').val();
     $('#complaint').val("");
-
-      let promise = new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      let url =
-      `https://api.betterdoctor.com/2016-03-01/doctors?query=${complaint}&location=45.4814320%2C-122.8016600%2C50&sort=rating-asc&skip=0&limit=40&user_key=${apiKey}`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      };
-      request.open("GET", url, true);
-      request.send();
-    });
-
-    promise.then(function(response) {
-      let body = JSON.parse(response);
-      let docArray = []
-
-      body.data.forEach(function(doc) {
-      let name = `${doc.profile.first_name} ${doc.profile.last_name}, ${doc.profile.title}`
-      let address = doc.practices[0].visit_address
-      let formattedAddress =  `${address.street} <br> ${address.city} <br> ${address.state}, ${address.zip}`
-      let phone = `${doc.practices[0].phones.find(function(phone) {return phone.type === "landline"}).number}`
-
-
-        docArray.push({
-          name: name,
-          address: formattedAddress,
-          phone: phone,
-          })
-        })
-
-
-      console.log(docArray)
-      $('#showDoctorNames').text("")
-      if(body.data.length != 0) {
-        $('.showDoctors').text(`Here are some doctors that treat ${complaint}.`);
-
-        docArray.forEach(function(doctor) {
-          $('#showDoctorNames').append(`<li> <b>${doctor.name}</b> <br> ${doctor.address} <br> ${doctor.phone} </li>`)
-          })
-
-        } else {
-          $(".showDoctors").text(`There are no doctors that treat ${complaint}, please modify your search terms`);
-        }
-
-      }, function(error) {
-        $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-    });
+    let doctor = new Doctor;
+    doctor.apiRequest(complaint, apiKey)
   });
 });
